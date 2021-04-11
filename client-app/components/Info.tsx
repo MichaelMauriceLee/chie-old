37fc217b4baa9a6ccb7c3ddf96757ea8b12e0315
Michael Lee <m.maurice.lee@gmail.com>
2021-04-10 19:43:53 -0600

    Improved info text

--- client-app/components/Info.tsx
+++ client-app/components/Info.tsx
@@ -3,6 +3,23 @@ import React from 'react';
 const Info: React.FC = () => (
   <div className="mt-2 space-y-4">
     <h3 className="text-2xl font-bold">
+      Welcome
+    </h3>
+    <div>
+      Chie is a next generation Japanese-English dictionary aimed towards
+      English speakers learning Japanese.
+    </div>
+    <div>
+      Chie supports first class intergration with Anki,
+      a free software that allows you to learn new words in a language easier via flashcards.
+    </div>
+    <div>
+      In addition to being able to look up words either in English
+      or various forms of Japanese (romanji, kanji, kana) and add them to Anki,
+      you can lookup words in images, as well as transcribe audio using AI.
+    </div>
+
+    <h3 className="text-2xl font-bold">
       Setup
     </h3>
     <div>
@@ -29,6 +46,33 @@ const Info: React.FC = () => (
     <div>
       You can now add cards to Anki using this site.
       Make sure to keep Anki running in the background.
+    </div>
+
+    <h3 className="text-2xl font-bold">
+      Contributions
+    </h3>
+    <div>
+      Chie is a free open source web app.&nbsp;
+      If you would like to contribute some code,
+      make suggestions or report any bugs, please click&nbsp;
+      <a
+        className="text-blue-500 visited:text-purple-500 rounded focus:outline-none focus:ring focus:border-blue-500"
+        href="https://github.com/MichaelMauriceLee/chie"
+      >
+        here
+      </a>
+      .
+    </div>
+    <div>
+      This site is using the Jisho.org API in order to provide english definitions.&nbsp;
+      If you would like to support them, please click&nbsp;
+      <a
+        className="text-blue-500 visited:text-purple-500 rounded focus:outline-none focus:ring focus:border-blue-500"
+        href="https://jisho.org/faq"
+      >
+        here
+      </a>
+      .
     </div>
   </div>
 );

aede5f2a552a123463613c7f9b08c46a4c045ce4
Michael Lee <m.maurice.lee@gmail.com>
2021-04-10 20:13:41 -0600

    Added clearable icon to search bar

--- client-app/components/Notifications/Notification.tsx
+++ client-app/components/Notifications/Notification.tsx
@@ -65,11 +65,13 @@ const Notification: React.FC<NotificationProps> = ({
         <p className="text-white ml-2">{message}</p>
 
         <button
-          className="bg-blue-200 hover:bg-blue-500 text-white rounded-full h-2 w-2 flex items-center justify-center mr-2 p-4"
+          className="bg-blue-200 hover:bg-blue-500 text-white rounded-full flex items-center justify-center mr-2"
           type="button"
           onClick={handleCloseNotification}
         >
-          <span>X</span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+          </svg>
         </button>
       </div>
 
--- client-app/components/SearchBar.tsx
+++ client-app/components/SearchBar.tsx
@@ -42,25 +42,36 @@ const SearchBar: React.FC<SearchBarProps> = ({
   return (
     <div className="flex flex-row justify-between items-center mt-4">
       <div className="outline relative border-2 focus-within:border-blue-500 flex-grow">
-        <div>
-          <input
-            className="block text-lg p-4 w-full appearance-none focus: outline-none bg-transparent"
-            id="searchInput"
-            type="text"
-            placeholder=" "
-            ref={searchBarRef}
-            value={keyword}
-            onChange={onInputChange}
-            onKeyUp={onKeyUp}
-          />
+        <input
+          className="block text-lg p-4 w-full appearance-none focus: outline-none bg-transparent"
+          id="searchInput"
+          type="text"
+          placeholder=" "
+          ref={searchBarRef}
+          value={keyword}
+          onChange={onInputChange}
+          onKeyUp={onKeyUp}
+        />
 
-          <label
-            className="absolute p-4 text-lg top-0 duration-300 -z-1 origin-0 bg-white"
-            htmlFor="searchInput"
+        <label
+          className="absolute p-4 text-lg top-0 duration-300 -z-1 origin-0 bg-white"
+          htmlFor="searchInput"
+        >
+          Search
+        </label>
+
+        {keyword && (
+          <button
+            className="rounded-full absolute right-5 top-1/2 transform -translate-y-2/4 z-10
+          hover:text-blue-500 focus:outline-none focus:ring focus:border-blue-500"
+            type="button"
+            onClick={() => { setKeyword(''); }}
           >
-            Search
-          </label>
-        </div>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+            </svg>
+          </button>
+        )}
       </div>
 
       <button

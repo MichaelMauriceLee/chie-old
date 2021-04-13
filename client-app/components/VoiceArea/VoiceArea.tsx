bc32836c071ced0cb2e9b7177de5c72c8796deb8
Michael Lee <m.maurice.lee@gmail.com>
2021-04-12 21:59:38 -0600

    Added continous speech to text

--- client-app/components/VoiceArea/VoiceArea.tsx
+++ client-app/components/VoiceArea/VoiceArea.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import useSpeechToText from '../../hooks/useSpeechToText';
 
 const VoiceArea: React.FC = () => {
-  const { displayText, fetchTextFromFile, fetchTextFromMic } = useSpeechToText();
+  const {
+    displayText,
+    tempDisplayText,
+    fetchTextFromFile,
+    fetchTextFromMic,
+    stopMicRecording,
+    isRecognizingMic,
+    isRecognizingFile,
+  } = useSpeechToText();
 
   const onClick = () => {
+    if (isRecognizingMic) {
+      stopMicRecording();
+      return;
+    }
     fetchTextFromMic();
   };
 
@@ -19,22 +31,72 @@ const VoiceArea: React.FC = () => {
     <div className="flex mt-2 border rounded">
       <div className="p-4 w-30">
         <button className="flex flex-row items-center hover:text-blue-500" type="button" onClick={onClick}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
-          </svg>
-          <div>
-            Use Microphone
-          </div>
+          { isRecognizingMic ? (
+            <svg
+              className="animate-spin h-5 w-5 text-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />
+            </svg>
+          ) : (
+            <>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
+              </svg>
+              <div>
+                Use Microphone
+              </div>
+            </>
+          )}
         </button>
 
         <div className="mt-2">
           <label className="flex flex-row hover:text-blue-500" htmlFor="audio-file">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-            </svg>
-            <div>
-              Upload audio file
-            </div>
+            { isRecognizingFile ? (
+              <svg
+                className="animate-spin h-5 w-5 text-blue-500"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+            ) : (
+              <>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                </svg>
+                <div>
+                  Upload audio file
+                </div>
+              </>
+            )}
           </label>
           <input
             type="file"
@@ -46,6 +108,7 @@ const VoiceArea: React.FC = () => {
       </div>
       <div className="h-96 p-1 overflow-auto">
         {displayText}
+        {tempDisplayText}
       </div>
     </div>
   );
--- client-app/hooks/useSpeechToText.ts
+++ client-app/hooks/useSpeechToText.ts
@@ -1,33 +1,63 @@
 import {
-  ResultReason, SpeechConfig, AudioConfig, SpeechRecognizer,
+  SpeechConfig, AudioConfig, SpeechRecognizer,
 } from 'microsoft-cognitiveservices-speech-sdk';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { getSpeechToken } from '../services/agent';
 
 interface UseSpeechToText {
   displayText: string;
+  tempDisplayText: string;
   fetchTextFromMic: () => Promise<void>;
   fetchTextFromFile: (audioFile: File) => Promise<void>;
+  stopMicRecording: () => void;
+  stopFileTranscription: () => void;
+  isRecognizingMic: boolean;
+  isRecognizingFile: boolean;
 }
 
 const useSpeechToText = (): UseSpeechToText => {
   const [displayText, setDisplayText] = useState('Upload an audio file or speak into your microphone');
+  const [tempDisplayText, setTempDisplayText] = useState('');
+  const [isRecognizingMic, setIsRecognizingMic] = useState(false);
+  const [isRecognizingFile, setIsRecognizingFile] = useState(false);
+  const micRecognizerInstance = useRef<SpeechRecognizer | null>(null);
+  const fileRecognizerInstance = useRef<SpeechRecognizer | null>(null);
+
+  const stopMicRecording = () => {
+    setIsRecognizingMic(false);
+    micRecognizerInstance.current = null;
+  };
+
+  const stopFileTranscription = () => {
+    setIsRecognizingFile(false);
+    fileRecognizerInstance.current = null;
+  };
 
   const fetchTextFromMic = async () => {
     const { token, region } = await getSpeechToken();
     const speechConfig = SpeechConfig.fromAuthorizationToken(token, region);
     speechConfig.speechRecognitionLanguage = 'ja-JP';
+    speechConfig.enableDictation();
     const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
     const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-    setDisplayText('Speak into your microphone...');
-
-    recognizer.recognizeOnceAsync((result) => {
-      if (result.reason === ResultReason.RecognizedSpeech) {
-        setDisplayText(`RECOGNIZED: ${result.text}`);
-        return;
-      }
-      setDisplayText('ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.');
-    });
+    micRecognizerInstance.current = recognizer;
+    if (micRecognizerInstance.current) {
+      setDisplayText('Speak into your microphone...');
+      micRecognizerInstance.current.recognizing = (_, event) => {
+        setTempDisplayText(event.result.text);
+      };
+      micRecognizerInstance.current.recognized = (_, event) => {
+        setDisplayText((prev) => prev + event.result.text);
+        setTempDisplayText('');
+      };
+      micRecognizerInstance.current.startContinuousRecognitionAsync(
+        () => { setIsRecognizingMic(true); },
+        () => {
+          setDisplayText('ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.');
+          setIsRecognizingMic(false);
+        },
+      );
+    }
   };
 
   const fetchTextFromFile = async (audioFile: File) => {
@@ -35,27 +65,44 @@ const useSpeechToText = (): UseSpeechToText => {
       setDisplayText('ERROR: File must be .wav sound file.');
       return;
     }
-    setDisplayText(`${audioFile.name} size=${audioFile.size} bytes`);
-
     const { token, region } = await getSpeechToken();
     const speechConfig = SpeechConfig.fromAuthorizationToken(token, region);
     speechConfig.speechRecognitionLanguage = 'ja-JP';
+    speechConfig.enableDictation();
     const audioConfig = AudioConfig.fromWavFileInput(audioFile);
     const recognizer = new SpeechRecognizer(speechConfig, audioConfig);
-
-    recognizer.recognizeOnceAsync((result) => {
-      if (result.reason === ResultReason.RecognizedSpeech) {
-        setDisplayText(`RECOGNIZED: ${result.text}`);
-        return;
-      }
-      setDisplayText('ERROR: Speech was cancelled or could not be recognized.');
-    });
+    fileRecognizerInstance.current = recognizer;
+    if (fileRecognizerInstance.current) {
+      setDisplayText(`${audioFile.name} size=${audioFile.size} bytes`);
+      fileRecognizerInstance.current.recognizing = (_, event) => {
+        setTempDisplayText(event.result.text);
+      };
+      fileRecognizerInstance.current.recognized = (_, event) => {
+        setDisplayText((prev) => prev + event.result.text);
+        setTempDisplayText('');
+      };
+      fileRecognizerInstance.current.sessionStopped = () => {
+        setIsRecognizingFile(false);
+      };
+      fileRecognizerInstance.current.startContinuousRecognitionAsync(
+        () => { setIsRecognizingFile(true); },
+        () => {
+          setDisplayText('ERROR: Speech was cancelled or could not be recognized.');
+          setIsRecognizingFile(false);
+        },
+      );
+    }
   };
 
   return {
     displayText,
+    tempDisplayText,
     fetchTextFromMic,
     fetchTextFromFile,
+    stopMicRecording,
+    stopFileTranscription,
+    isRecognizingMic,
+    isRecognizingFile,
   };
 };
 

73c376ad88efb3d6ca6195eaeaff7ea54ac6007e
Michael Lee <m.maurice.lee@gmail.com>
2021-11-27 21:57:38 -0700

    Build fix

--- client-app/hooks/useCreateNote.ts
+++ client-app/hooks/useCreateNote.ts
@@ -8,7 +8,8 @@ const addNoteToCurrentDeck = async (card: Note) => {
   try {
     const data = await postNote(card);
     return data;
-  } catch (error) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
     throw new Error(error.message ?? 'Failed to create note');
   }
 };
